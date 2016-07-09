ca1dc009ead467de4c1bd3c95c90c6e684103160
Jethro Beekman <jethro@jbeekman.nl>
2016-07-08 21:53:56 -0700

    Add feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![allow(stable_features,unused_features)]
-#![feature(collections,alloc,unsafe_no_drop_flag,dropck_parametricity,collections_bound,heap_api,core_intrinsics,unique,oom,filling_drop,sip_hash_13)]
+#![feature(collections,alloc,unsafe_no_drop_flag,dropck_parametricity,collections_bound,heap_api,core_intrinsics,unique,oom,filling_drop,sip_hash_13,hashmap_hasher)]
 #![no_std]
 
 #[macro_use]
