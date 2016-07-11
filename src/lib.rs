778ad5226919e6bfba9fbd484cba84e7f591c224
Jethro Beekman <jethro@jbeekman.nl>
2016-07-10 19:44:04 -0700

    Add rand feature

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,18 @@
 #[macro_use]
 extern crate collections as core_collections;
 extern crate alloc;
+#[cfg(feature="rand")]
 extern crate rand;
+
+// Needed for older compilers, to ignore thread_local!/println! macros in tests
+macro_rules! thread_local {
+    (static $name:ident: $t:ty = $init:expr) => { };
+    (pub static $name:ident: $t:ty = $init:expr) => { };
+}
+macro_rules! println {
+    ($fmt:expr) => { () };
+    ($fmt:expr, $($arg:tt)*) => { () };
+}
 
 include!(concat!(env!("OUT_DIR"), "/collections.rs"));
 pub use collections::*;
